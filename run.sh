2568c828077280185508e1c027b8e535ef1c9f80
andy <andrejbalaz001@gmail.com>
2021-05-07 12:14:19 +0200

    finished run script

--- run.sh
+++ run.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 
-fasta='data/test.fna'
-outdir='prokka'
-sample_name='test'
+fasta=${1}
+outdir='result'
+
+tmp=${fasta%.*}
+sample_name=${tmp##*/}
+
+echo ${sample_name}
 
 mkdir -p ${outdir}
-    
+
+echo "Predicting genes with prokka."
 prokka                                      \
     --outdir ${outdir}                      \
     --force                                 \
@@ -23,14 +28,16 @@ prokka                                      \
 # create index with:
 # makeblastdb -in 008_train.cd-hit.genes.faa -dbtype prot
 
+echo "Finding clusters"
 blastp                                              \
     -query ${outdir}/${sample_name}.faa             \
     -db data/008_train.cd-hit.genes.faa             \
     -out ${outdir}/${sample_name}.blast             \
     -outfmt "6 qseqid sseqid score pident evalue"   \
     -max_hsps 1	                                    \
-    -max_target_seqs 1
+    -max_target_seqs 1	2> /dev/null
     
+echo "Predicting host"
 python ./main.py    \
     --blast_file ${outdir}/${sample_name}.blast \
     --clusters_file data/010_mcl.clusters  	\
